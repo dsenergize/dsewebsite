6ff1724c3e0fe57c3dbee4ce81501804313e1ba0
abid usmani <abidusmani343@gmail.com>
2025-09-24 18:55:03 +0530

    Remove backend server and routes; update frontend API URLs to point to the deployed Strapi instance

--- src/pages/resources/Blog.tsx
+++ src/pages/resources/Blog.tsx
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import Footer from '@/components/Footer';
 
 // --- Configuration ---
-const API_URL = 'http://localhost:1337/api/blog-posts?populate=*';
+const API_URL = 'https://strapi-dsewebsite-d05s.onrender.com/api/blog-posts?populate=*';
 
 // --- SVG Icons ---
 const LinkedinIcon = () => ( <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"> <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /> </svg> );
@@ -38,7 +38,7 @@ const Blog = () => {
         const normalizedApiPosts = validApiPosts.map(post => {
           const imageUrlPath = post.Image?.formats?.large?.url || post.Image?.url;
           const imageUrl = imageUrlPath
-            ? `http://localhost:1337${imageUrlPath}`
+            ? `https://strapi-dsewebsite-d05s.onrender.com${imageUrlPath}`
             : 'https://placehold.co/800x400';
 
           return {
